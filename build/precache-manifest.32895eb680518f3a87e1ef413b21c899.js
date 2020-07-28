d55d85f3c42ab24253da1989e26799eafcb9367e
Nazaqat <naa.dk@yahoo.com>
2020-07-28 21:57:39 +0200

    Add files via upload

---
+++ build/precache-manifest.32895eb680518f3a87e1ef413b21c899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9201b23af3ca7acec6e19bd57c7e005f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "074df2d1a0c5a2b0860c",
+    "url": "/static/css/2.50f99185.chunk.css"
+  },
+  {
+    "revision": "39d213907cff86402497",
+    "url": "/static/css/main.72b42ed3.chunk.css"
+  },
+  {
+    "revision": "074df2d1a0c5a2b0860c",
+    "url": "/static/js/2.4fcc1f0d.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.4fcc1f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d213907cff86402497",
+    "url": "/static/js/main.777f86bb.chunk.js"
+  },
+  {
+    "revision": "1726c274e9430f55a346",
+    "url": "/static/js/runtime-main.ea3b1d25.js"
+  }
+]);
